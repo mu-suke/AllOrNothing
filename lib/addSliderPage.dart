0c2b6cb19ab92975fdc3b76467b61fa48485eb66
mu-suke <chelsea_08ym@yahoo.co.jp>
2020-03-11 00:19:40 +0900

    タイトル部分のバリデーション追加

--- lib/addSliderPage.dart
+++ lib/addSliderPage.dart
@@ -1,5 +1,4 @@
 import 'package:all_or_nothing_slider/components/constText.dart';
-import 'package:all_or_nothing_slider/components/myCustomFrom.dart';
 import 'package:all_or_nothing_slider/mySliderClass.dart';
 import 'package:flutter/material.dart';
 import 'mySliderClass.dart';
@@ -20,48 +19,57 @@ class _AddSliderPageState extends State<AddSliderPage> {
 
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
     return Scaffold(
       appBar: AppBar(title: Text(ConstText.sliderCreateView)),
-      body: Container(
-          padding: const EdgeInsets.all(50.0),
-          child: Column(
-            children: <Widget>[
-              TextField(
-                controller: _textController,
-              ),
-              MyCustomForm(),
-              Container(
-                padding: const EdgeInsets.all(50.0),
-                child: Column(
-                  children: <Widget>[
-                    Center(child: Text("現在の値：${_value.toInt()}")),
-                    new Slider(
-                      label: '${_value.toInt()}',
-                      min: 0,
-                      max: 100,
-                      value: _value,
-                      activeColor: Colors.orange,
-                      inactiveColor: Colors.blueAccent,
-                      divisions: 20,
-                      onChanged: _changeSlider,
-                    )
-                  ],
-                ),
-              ),
-              RaisedButton.icon(
-                  onPressed: () {
-                    _newSlider.title = _textController.text.toString();
-                    _newSlider.value = _value.toInt();
-                    Navigator.of(context).pop(_newSlider);
+      body: Form(
+        key: _formKey,
+        child: Container(
+            padding: const EdgeInsets.all(50.0),
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'テキストを入力してください。';
+                    }
                   },
-                  icon: Icon(
-                    Icons.check_circle_outline,
-                    color: Colors.white,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(50.0),
+                  child: Column(
+                    children: <Widget>[
+                      Center(child: Text("現在の値：${_value.toInt()}")),
+                      new Slider(
+                        label: '${_value.toInt()}',
+                        min: 0,
+                        max: 100,
+                        value: _value,
+                        activeColor: Colors.orange,
+                        inactiveColor: Colors.blueAccent,
+                        divisions: 20,
+                        onChanged: _changeSlider,
+                      )
+                    ],
                   ),
-                  label: Text("決定")
-              )
-            ],
-          ),
+                ),
+                RaisedButton.icon(
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        _newSlider.title = _textController.text.toString();
+                        _newSlider.value = _value.toInt();
+                        Navigator.of(context).pop(_newSlider);
+                      }
+                    },
+                    icon: Icon(
+                      Icons.check_circle_outline,
+                      color: Colors.white,
+                    ),
+                    label: Text("決定")
+                )
+              ],
+            ),
+        ),
       ),
     );
   }
