08f628f739021c467bb4d546aab66244294e6722
mu-suke <chelsea_08ym@yahoo.co.jp>
2020-03-14 23:58:44 +0900

    長押しで削除機能実装

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: ConstText.appTitle,
       theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
-        primaryTextTheme: TextTheme(
-          title: TextStyle(
-            color: Colors.white
-          )
-        )
-      ),
+          primarySwatch: Colors.lightBlue,
+          primaryTextTheme: TextTheme(title: TextStyle(color: Colors.white))),
       home: MyHome(),
     );
   }
@@ -32,47 +27,57 @@ class MyHome extends StatefulWidget {
 }
 
 Future getData(String collection, String documentId, String fieldName) async {
-  DocumentSnapshot docSnapshot =
-  await Firestore.instance.collection(collection).document(documentId).get();
+  DocumentSnapshot docSnapshot = await Firestore.instance
+      .collection(collection)
+      .document(documentId)
+      .get();
   Map record = docSnapshot.data;
   return record[fieldName];
 }
-final snapshot =  getData('users', 'test_data', 'title');
-class _MyHomeState extends State<MyHome> {
 
+final snapshot = getData('users', 'test_data', 'title');
+
+class _MyHomeState extends State<MyHome> {
   List<MySlider> _sliderList = [
     MySlider(title: 'hogehoge', value: 40, createdAt: DateTime.now()),
-    MySlider(title: getData('users', 'test_data', 'title').toString(), value: 77, createdAt: DateTime.now())
+    MySlider(
+        title: getData('users', 'test_data', 'title').toString(),
+        value: 77,
+        createdAt: DateTime.now())
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(ConstText.appTitle),
+      appBar: AppBar(
+        title: Text(ConstText.appTitle),
+      ),
+      body: createListView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) {
+            return AddSliderPage();
+          }));
+        },
+        tooltip: 'Increment',
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
         ),
-        body: createListView(),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            await Navigator.of(context)
-                .push(MaterialPageRoute(builder: (context) {
-              return AddSliderPage();
-            }));
-          },
-          tooltip: 'Increment',
-          child: Icon(
-            Icons.add,
-            color: Colors.white,
-          ),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      );
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+    );
   }
+
   createListView() {
     return StreamBuilder(
-      stream: Firestore.instance.collection('users').orderBy('createdAt', descending: false).snapshots(),
+      stream: Firestore.instance
+          .collection('users')
+          .orderBy('createdAt', descending: false)
+          .snapshots(),
       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if(snapshot.hasError) {
+        if (snapshot.hasError) {
           return Text('Error: ${snapshot.error}');
         }
         switch (snapshot.connectionState) {
@@ -80,16 +85,16 @@ class _MyHomeState extends State<MyHome> {
             return Text('Loading...');
           default:
             return ListView(
-              children: snapshot.data.documents.map((DocumentSnapshot document) {
+              children:
+                  snapshot.data.documents.map((DocumentSnapshot document) {
                 return Container(
                   decoration: BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(color: Colors.black38)
-                    )
-                  ),
+                      border:
+                          Border(bottom: BorderSide(color: Colors.black38))),
                   child: new ListTile(
                     trailing: Visibility(
-                      visible: _isCreatedAtCheck(document['createdAt'].toDate()),
+                      visible:
+                          _isCreatedAtCheck(document['createdAt'].toDate()),
                       child: new Icon(
                         Icons.fiber_new,
                         color: Colors.lightBlue,
@@ -100,9 +105,18 @@ class _MyHomeState extends State<MyHome> {
                     subtitle: new Text(document['value'].toString()),
                     onTap: () {
                       Scaffold.of(context).showSnackBar(new SnackBar(
-                        content: new Text('Created at ${timeAgo.format(document['createdAt'].toDate()).toString()}'),
+//                        content: new Text('Created at ${timeAgo.format(document['createdAt'].toDate()).toString()}'),
+                        content: new Text(document.documentID),
                         duration: Duration(seconds: 1),
                       ));
+                    },
+                    onLongPress: () {
+                      _showDialog().then((value) {
+                        if(value) {
+                          Firestore.instance.collection('users').document(document.documentID).delete();
+                          print("document deleted");
+                        }
+                      });
                     },
                   ),
                 );
@@ -112,14 +126,37 @@ class _MyHomeState extends State<MyHome> {
       },
     );
   }
+
+  Future _showDialog() {
+    return showDialog<bool>(
+      context: context,
+      builder: (BuildContext context) => new AlertDialog(
+        title: new Text('AlertDialog'),
+        content: new Text('アラートダイアログです。YesかNoを選択してください。'),
+        actions: <Widget>[
+          new SimpleDialogOption(
+            child: new Text('No'),
+            onPressed: () {
+              Navigator.of(context).pop(false);
+            },
+          ),
+          new SimpleDialogOption(
+            child: new Text('Yes'),
+            onPressed: () {
+              Navigator.of(context).pop(true);
+            },
+          ),
+        ],
+      ),
+    );
+  }
 }
-// TODO: 長押しで削除機能
-// TODO: NEWマーク1週間以内のみ表示
 
 bool _isCreatedAtCheck(DateTime createdAt) {
   const _oneWeek2Second = 7 * 24 * 60 * 60;
   final _createdAtSecond = createdAt.millisecondsSinceEpoch / 1000;
   final _clockSecond = new DateTime.now().millisecondsSinceEpoch / 1000;
-  final bool _isWithin1week =  _clockSecond - _createdAtSecond < _oneWeek2Second;
+  final bool _isWithin1week = _clockSecond - _createdAtSecond < _oneWeek2Second;
   return _isWithin1week;
 }
+
